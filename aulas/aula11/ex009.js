b9397b47b8443e326bbfc969e90685868a06a90c
Cleber S. Lopes Jr <47905314+cleberslopesjr@users.noreply.github.com>
2020-05-20 00:32:59 -0300

    Commit

--- aulas/aula11/ex009.js
+++ aulas/aula11/ex009.js
@@ -1,6 +1,6 @@
 var pais = 'EUA'
-console.log('Vivendo em ${pais}')
-if(pais == 'Brasil'){
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
     console.log('Você é Brasileiro')
 } else {
     console.log('Você é Estrangeiro!')
